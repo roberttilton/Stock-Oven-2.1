d7ee4a792c7124b9850c7f6dc73a485208c3429d
Robert Tilton <robtilton2086@gmail.com>
2021-04-13 14:30:41 -0400

    rendering jsx for reddit api call FINALLY

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import API from "../utils/API";
 
 function Home() {
@@ -10,34 +10,40 @@ function Home() {
     "YOLO": [],
     "Daily Discussion": [],
     "Chart": []
-  })
+  });
+
+  useEffect(() => {
+    getReddit();
+  }, []);
 
   const getReddit = () => {
     API.getReddit()
       .then((data) => {
+        console.log(data);
+        const redditData = data.data.data;
         const postObject = {
-          // "DD": [],
-          // "Gain": [],
-          // "Loss": [],
-          // "News": [],
-          // "YOLO": [],
-          // "Daily Discussion": [],
-          // "Chart": []
+          "DD": [],
+          "Gain": [],
+          "Loss": [],
+          "News": [],
+          "YOLO": [],
+          "Daily Discussion": [],
+          "Chart": []
         };
-        for (let i = 0; i < data.children.length; i++) {
-          const currentFlair = data.children[i].data.link_flair_text;
+        for (let i = 0; i < redditData.children.length; i++) {
+          const currentFlair = redditData.children[i].data.link_flair_text;
           const dataObject = {};
-          if (data.children[i].data.title) {
-            dataObject["title"] = data.children[i].data.title
+          if (redditData.children[i].data.title) {
+            dataObject["title"] = redditData.children[i].data.title
           }
-          if (data.children[i].data.author) {
-            dataObject["author"] = data.children[i].data.author
+          if (redditData.children[i].data.author) {
+            dataObject["author"] = redditData.children[i].data.author
           }
-          if (data.children[i].data.permalink) {
-            dataObject["link"] = data.children[i].data.permalink
+          if (redditData.children[i].data.permalink) {
+            dataObject["link"] = redditData.children[i].data.permalink
           }
-          if (data.children[i].data.score) {
-            dataObject["score"] = data.children[i].data.score
+          if (redditData.children[i].data.score) {
+            dataObject["score"] = redditData.children[i].data.score
           }
           if (Object.keys(postObject).includes(currentFlair)) {
             postObject[currentFlair].push(dataObject);
@@ -56,61 +62,120 @@ function Home() {
       });
   };
 
-  handleFormSubmit = (event) => {
-    event.preventDefault();
-    getBooks();
-  };
-
-  handleBookSave = (id) => {
-    const book = this.state.books.find((book) => book.id === id);
-
-    API.saveBook({
-      googleId: book.id,
-      title: book.volumeInfo.title,
-      subtitle: book.volumeInfo.subtitle,
-      link: book.volumeInfo.infoLink,
-      authors: book.volumeInfo.authors,
-      description: book.volumeInfo.description,
-      image: book.volumeInfo.imageLinks.thumbnail,
-    }).then(() => this.getBooks());
-  };
-
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col size="md-12">
-            <Jumbotron>
-              <h1 className="text-center">
-                <strong>(React) Google Books Search</strong>
-              </h1>
-              <h2 className="text-center">
-                Search for and Save Books of Interest.
-              </h2>
-            </Jumbotron>
-          </Col>
-          <Col size="md-12">
-            <Card title="Book Search" icon="far fa-book">
-              <Form
-                handleInputChange={/* YOUR CODE HERE */}
-                handleFormSubmit={/* YOUR CODE HERE */}
-                q={/* YOUR CODE HERE */}
-              />
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-12">
-            <Card title="Results">
-              {/* Render the arrays of books in this.state. If the array is empty, display the message in this.state */}
-              {/* YOUR CODE HERE */}
-            </Card>
-          </Col>
-        </Row>
-        <Footer />
-      </Container>
-    );
-  }
+  return (
+    <div className="tile is-parent is-7">
+				<div className="tile notification is-danger" id="reddit-container">
+					<div className="sort-buttons">
+						<button className="sort-button" id="reddit-dd" type="button" style={{backgroundColor: "#365b8c"}} data-tip="Due Diligence">DD</button>
+						<button className="sort-button" id="reddit-gain" type="button" style={{backgroundColor: "green"}} data-tip="Reported gains">Gain</button>
+						<button className="sort-button" id="reddit-loss" type="button" style={{backgroundColor: "crimson"}} data-tip="Reported losses">Loss</button>
+						<button className="sort-button" id="reddit-news" type="button" style={{backgroundColor: "goldenrod"}} data-tip="Market news">News</button>
+						<button className="sort-button" id="reddit-yolo" type="button" style={{backgroundColor: "cadetblue"}} data-tip="Risky positions">YOLO</button>
+						<button className="sort-button" id="reddit-discussion" type="button" style={{backgroundColor: "#b43fc4"}} data-tip="Discussion">Discussion</button>
+						<button className="sort-button" id="reddit-chart" type="button" style={{backgroundColor: "orangered"}} data-tip="Chart">Chart</button>
+					</div>
+					<div id="reddit">
+						<div className="no-reddit tooltip"></div>
+            {reddit.DD.length ? (
+              <ul>
+                {reddit.DD.map((post) => (
+                  <li>
+                        <p>{post.title}</p>
+                        <p>{post.author}</p>
+                        <p>{post.score}</p>
+                        <p>{post.link}</p>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+            {/* {reddit.Gain.length ? (
+              <List>
+                {reddit.map(book => (
+                  <ListItem key={book._id}>
+                    <Link to={"/books/" + book._id}>
+                      <strong>
+                        {book.title} by {book.author}
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => deleteBook(book._id)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+            {reddit.Loss.length ? (
+              <List>
+                {reddit.map(book => (
+                  <ListItem key={book._id}>
+                    <Link to={"/books/" + book._id}>
+                      <strong>
+                        {book.title} by {book.author}
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => deleteBook(book._id)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+            {reddit["Daily Discussion"].length ? (
+              <List>
+                {reddit.map(book => (
+                  <ListItem key={book._id}>
+                    <Link to={"/books/" + book._id}>
+                      <strong>
+                        {book.title} by {book.author}
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => deleteBook(book._id)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+            {reddit.DD.length ? (
+              <List>
+                {reddit.map(book => (
+                  <ListItem key={book._id}>
+                    <Link to={"/books/" + book._id}>
+                      <strong>
+                        {book.title} by {book.author}
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => deleteBook(book._id)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+            {reddit.DD.length ? (
+              <List>
+                {reddit.map(book => (
+                  <ListItem key={book._id}>
+                    <Link to={"/books/" + book._id}>
+                      <strong>
+                        {book.title} by {book.author}
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => deleteBook(book._id)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )} */}
+					</div>
+				</div>
+			</div>
+  );
 }
 
-export default Home;
+
+
+  export default Home;

bb06cf84b780aa28566429055c5133dceb844237
Robert Tilton <robtilton2086@gmail.com>
2021-04-16 13:02:00 -0400

    added a bunch of the boilerplate for login/logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,35 +1,41 @@
 const db = require("../models");
 
-// Defining methods for the bookController
+// Defining methods for the userController
 module.exports = {
   findAll: function(req, res) {
-    db.User.find({})
-      .then(dbUser => res.json(dbUser))
+    db.User
+      .find({})
+      .sort({ date: -1 })
+      .then(dbResults => res.json(dbResults))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.User.findById(req.params.id)
-      .then(dbUser => res.json(dbUser))
+    db.User
+      .findById(req.params.id)
+      .then(dbResults => res.json(dbResults))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    console.log(req.body);
-    db.User.create(req.body)
-      .then(dbUser => {
-        console.log(dbUser);
-        return res.json(dbUser)
+    console.log("controller - create: ", req.body);
+    db.User
+      .create(req.body)
+      .then(dbResults => {
+        // res.redirect(307, "/login");
+        res.json(dbResults);
       })
-      .catch(err => res.status(422).json(err));
+      .catch(err => res.status(401).json(err));
   },
   update: function(req, res) {
-    db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbUser => res.json(dbUser))
+    db.User
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbResults => res.json(dbResults))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.User.findById(req.params.id)
-      .then(dbUser => dbUser.remove())
-      .then(dbUser => res.json(dbUser))
+    db.User
+      .findById({ _id: req.params.id })
+      .then(dbResults => dbResults .remove())
+      .then(dbResults => res.json(dbResults))
       .catch(err => res.status(422).json(err));
   }
 };
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,8 @@ const path = require("path");
 const router = require("express").Router();
 const userRoutes = require("./users");
 const redditRoutes = require("./reddit");
+const signupRoutes = require("./signup");
+const passport = require("../../passport");
 
 // User routes 
 // endpoint: /api
@@ -10,6 +12,42 @@ router.use("/users", userRoutes);
 // Reddit routes
 // endpoint: /api
 router.use("/reddit", redditRoutes);
+
+router.use("/signup", signupRoutes);
+
+// Matches: /api/login
+router.post("/login", passport.authenticate("local"), (req, res) => {
+  console.log("POST /api/login - req.user: ", req.user);
+  const userInfo = {
+    id: req.user._id,
+    name: req.user.name,
+    email: req.user.email
+  };
+  res.json(userInfo);
+});
+
+// Matches: /api/login
+router.get("/login", (req, res) => {
+  console.log("GET /api/login, req.user: ", req.user);
+  if (!req.user) {
+    console.log("req.user does not exist");
+    res.send("Not logged in yet!");
+    // res.redirect("/login");
+  }
+  else {
+    return res.json(req.user.name);
+  }
+});
+
+// Matches /api/logout
+router.post("/logout", (req, res) => {
+  if (req.user) {
+    req.logout();
+    res.send({ message: "Logged out!" });
+  } else {
+    res.send({ message: "Not signed in!" });
+  }
+});
 
 // For anything else, render the html page
 router.use(function(req, res) {

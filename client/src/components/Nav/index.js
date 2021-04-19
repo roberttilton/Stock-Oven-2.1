67657baf248846a504e644b31663ba84ae958df5
Robert Tilton <robtilton2086@gmail.com>
2021-04-18 20:50:14 -0400

    added logo to the page, as well as got more styling done

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../../assets/stockoven2.2.png"
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
       <Link className="navbar-brand" to="/">
         StockOven
       </Link>
-
+      <img src={Logo} alt="Logo" id="logo"/>
       <Link className="nav-link" to="/home">
         Home
       </Link>
